9f9781c8d324638810ea93d444dc2eeda98d9e14
sakadayo <sakadayo1210@outlook.jp>
2026-01-18 09:36:06 +0900

    For Test about adding confirm or alert

--- scratch-virtual-exchange/index.js
+++ scratch-virtual-exchange/index.js
@@ -2,6 +2,10 @@ const client_id = 'gfmucqaudg8w80g';
 const redirect_uri = 'https://sakadayo.github.io/scratch-virtual-exchange';
 class Update{
     connect(){
+        const ok = confirm(
+        "Dropbox の認証画面が開きます。\nこれは安全な公式の認証手順です。\n続行してもよろしいですか？"
+        );
+        if (!ok) return;
         console.log(client_id);
         console.log(redirect_uri);
         console.log(`https://www.dropbox.com/oauth2/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code`);
