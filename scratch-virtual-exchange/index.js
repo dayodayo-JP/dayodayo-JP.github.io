64373992de230139ecc97868da1a62dec07bbd97
sakadayo <sakadayo1210@outlook.jp>
2026-01-18 21:51:09 +0900

    Finish feature to get before access token

--- scratch-virtual-exchange/index.js
+++ scratch-virtual-exchange/index.js
@@ -1,21 +1,15 @@
 const client_id = 'gfmucqaudg8w80g';
 const redirect_uri = 'https://sakadayo.github.io/scratch-virtual-exchange';
-setTimeout(() => {
-    console.log("3秒経過");
-}, 3000);
-document.getElementById("alert").innerHTML = "";
-class Update{
-    connect(){
-        console.log(client_id);
-        console.log(redirect_uri);
-        console.log(`https://www.dropbox.com/oauth2/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code`);
-        window.open(`https://www.dropbox.com/oauth2/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code`, '_blank');
-        var a = location.search;
-        var result = code.split('=');
-        var code = result[1];
-        
+var login = 'false';
+function connect()
+{
+    if(login !== "true")
+    {
+        return 0;
     }
+    window.local.href(`https://www.dropbox.com/oauth2/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code`);
+    var query = location.search;
+    var result = query.split('=');
+    var code = result[1];
+    console.log(code);
 }
-
-var ins = new Update();
-ins.connect();
