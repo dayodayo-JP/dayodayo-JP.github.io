6a7bc5a66f5838665ab415137e159e72ffc39581
sakadayo <sakadayo1210@outlook.jp>
2026-03-07 21:35:03 +0900

    Deleted about Scratch

--- ScVM.js
+++
@@ -1,8 +0,0 @@
-var button = document.getElementByID("button");
-button.style.visibility = 'hidden';
-function change(){
-    var text = document.getElementByID("already?");
-    //ここに処理をするコードを書く
-    text.textContent = "ファイルは用意されました。ダウンロードボタンを押して下さい。";
-    button.style.visibility = 'visibility';
-}
